a063d5ca731ca807048ea573e9571e85e5d3b84a
sajid-khan-js <sajid.khan1@sainsburys.co.uk>
2020-12-01 22:04:00 +0000

    test: add test for Hellos function and ran gofmt

--- modules/greetings/greetings_test.go
+++ modules/greetings/greetings_test.go
@@ -1,41 +1,53 @@
 package greetings
 
 import (
-    "testing"
 	"regexp"
-	"fmt"
+	"testing"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking 
+// TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestHelloName(t *testing.T) {
-    name := "Sajid"
-    want := regexp.MustCompile(`\b`+name+`\b`)
-    msg, err := Hello("Sajid")
-    if !want.MatchString(msg) || err != nil {
-        t.Fatalf(`Hello("Sajid") = %q, %v, want match for %#q, nil`, msg, err, want)
-    }
+	name := "Sajid"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	greeting, err := Hello("Sajid")
+	if !want.MatchString(greeting) || err != nil {
+		t.Fatalf(`Hello("Sajid") = %q, %v, want match for %#q, nil`, greeting, err, want)
+	}
 }
 
 func TestHellosNames(t *testing.T) {
-	
-    //TODO add test to check list of names appears in outputted map
 
-    names := []string{"Sajid", "Bob", "Alice"}
-	//want := regexp.MustCompile(`^(?!(Sajid|Bob|Alice)$)(\w*)`)
-	msg, err := Hellos(names)
-	
-	fmt.Println(msg, err)
-    //if !want.Match(msg) || err != nil {
-    //    t.Fatalf(`Hello("Sajid") = %q, %v, want match for %#q, nil`, msg, err, want)
-    //}
+	// Names to test function with
+	names := []string{"Sajid", "Bob", "Alice"}
+
+	greetings, err := Hellos(names)
+
+	// For each name we input, we expect a greeting back, so let's ensure that works by comparing the length
+	namesLength := len(names)
+	greetingsLength := len(greetings)
+	if namesLength != greetingsLength || err != nil {
+		t.Fatalf(`names length = %v, greetings length = %v, greetings = %q, error = %v`, namesLength, greetingsLength, greetings, err)
+	}
+
+	// Loop over the inputted list of names, using each name as a key to acccess its corresponding value in the map.
+	// The corresponding value (i.e. greeting) should include our inputted name
+	for _, name := range names {
+
+		want := regexp.MustCompile(`\b` + name + `\b`)
+
+		if !want.MatchString(greetings[name]) || err != nil {
+			t.Fatalf(`Hello("%v") = %q, %v, want match for %#q, nil`, name, greetings[name], err, want)
+		}
+
+	}
 }
 
-// TestHelloEmpty calls greetings.Hello with an empty string, 
+// TestHelloEmpty calls greetings.Hello with an empty string,
 // checking for an error.
 func TestHelloEmpty(t *testing.T) {
-    msg, err := Hello("")
-    if msg != "" || err == nil {
-        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
-    }
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
 }
