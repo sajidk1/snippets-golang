0841453a63aadaf37eaedd5da2de78e3dd2a44ff
sajid-khan-js <sajid.khan1@sainsburys.co.uk>
2020-12-01 09:39:57 +0000

    fix: working submodules

--- modules/hello/hello.go
+++ modules/hello/hello.go
@@ -1,10 +1,14 @@
-package hello
+package main
 
 import (
     "fmt"
     "rsc.io/quote"
+    "github.com/sajid-khan-js/snippets-golang/modules/greetings"
 )
 
 func main() {
+    
     fmt.Println(quote.Go())
+    message := greetings.Hello("Sajid")
+    fmt.Println(message)
 }
